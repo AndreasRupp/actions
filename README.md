c1f79c8782215c8569e98ad6353715cd3a46d323
AndreasRupp <andreas.rupp@fau.de>
2022-03-25 15:56:20 +0200

    Auto-generate and publish pdf files from tex.

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# actions
+# GitHub Action: tex-to-pdf
+
+GitHub action that builds and deploys a PDF to create automatically updated PDFs.
+
+GitHub offers the opportunity to publish web pages. This can be used to publish the PDF, associated
+to your tex project. If you want to publish the PDF of the latest push (or merged pull request to
+the branch `main` of your repository, you can proceed as follows:
+
+- Create a repository to contain the PDF `auto_pages` with your account. Enable the GitHub
+  pages in its `Settings` and use the `main` branch to be published.
+
+- Create a dummy GitHub account `dummy_acc` and give it write access to `auto_pages`. The email
+  address with which the account ins created is `dummy_acc_mail`. Afterwards, create a personal
+  access token of `dummy_acc` providing read and write access to all repositories.
+
+- Store the personal access token as secret `CI_REPO_TOKEN` in the repository of which the PDF will
+  be created.
+
+- Add a file `tex-to-pdf.yml` to your repository's folder `.github/workflows`. This file might
+  contain
+
+```
+name: "TeX to PDF"
+
+on: push
+
+jobs:
+  make_and_deploy_pdf:
+    name: Make and deploy pdf
+    runs-on: ubuntu-20.04
+    steps:
+    - name: Checkout current branch
+      uses: actions/checkout@v2
+      with:
+        submodules: recursive
+    - name: Run build and deploy action
+      uses: AndreasRupp/actions@tex-to-pdf
+      with:
+        deploy_pdf: ${{ github.ref == 'refs/heads/main' }}
+        tex_command: "pdflatex"
+        bib_command: "bibtex"
+        ci_repo_token: ${{ secrets.CI_REPO_TOKEN }}
+        ci_user_name: dummy_acc
+        ci_user_mail: dummy_acc_mail
+        pages_repo_organization: your_user_name
+        pages_repo_name: auto_pages
+        tex_file: main
+```
+
+where `your_user_name` is the user/organization which owns the `auto_pages`, `pdflatex` is the
+command which builds the PDF if you are in the main directory of the rspository, and the 
+`tex_file` is the name of your main .tex file (without the .tex extension).
+
+The script will then try to build the PDF on all pushes and check, whether this can be done, but
+it will only be published if there is a successful push or merged pull-request to branch `main`.
---
+++ deploy_pdf.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+####################################################################################################
+# The original script is signed by Jeroen de Bruijn. A later version is signed by Francesco Romano.
+# This script is a further adaption of the script of Francesco Romano, which can be found under
+# https://gist.github.com/francesco-romano/351a6ae457860c14ee7e907f2b0fc1a5 (date: Dec. 20, 2020)
+#
+# Author: Andreas Rupp, Heidelberg University, 2021.
+####################################################################################################
+
+# Setup this script and get the current gh-pages branch.
+echo 'Setting up the script...'
+
+# Exit with nonzero exit code if anything fails.
+set -e
+
+# Pretend to be user GH_USER_NAME.
+git config --global user.name $GH_USER_NAME
+git config --global user.email $GH_USER_MAIL
+
+# Retrieve master branch of the repositoy containing the GitHub pages.
+rm -rf $GH_AUX_REP
+git clone https://$GH_USER_NAME:$REPO_TOKEN@github.com/$GH_REPO_ORG/$GH_REPO_NAME.git $GH_AUX_REP
+cd $GH_AUX_REP
+
+# Set the push default to simple i.e. push only the current branch.
+git config --global push.default simple
+
+# Go back to first commit.
+git reset --hard `git rev-list --max-parents=0 --abbrev-commit HEAD`
+
+# Create .nojekyll file.
+echo "" > .nojekyll
+
+# Overwrite index.html file.
+echo "Not to be displayed." > index.html
+
+# Copy doxygen into current branch.
+cp -r ../$PDF_FILE .
+
+# Only upload if Doxygen successfully created the documentation.
+if [ -f $PDF_FILE ]; then
+  echo 'Uploading documentation to the HyperHDG_pages repository...'
+  # Add everything in this directory (the Doxygen code documentation) to the gh-pages branch.
+  git add --all
+
+  # Commit the added files with a title and description containing the GitHub actions build number
+  # and the GitHub commit reference that issued this build.
+  git commit -m "Deploy PDF to GitHub master build: ${GITHUB_RUN_NUMBER}" \
+    -m "Commit: $(git rev-parse --short "$GITHUB_SHA")"
+
+  # Force push to the remote GitHub pages branch.
+  git push --force https://$GH_USER_NAME:$REPO_TOKEN@github.com/$GH_REPO_ORG/$GH_REPO_NAME.git
+else
+  echo '' >&2
+  echo 'Warning: No documentation (html) files have been found!' >&2
+  echo 'Warning: Not going to push the documentation to GitHub!' >&2
+  exit 1
+fi
